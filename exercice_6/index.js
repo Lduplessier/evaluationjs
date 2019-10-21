c941f213208f730da389c8cfdcba402e0072ff65
LDU <lucas.duplessier@gmail.com>
2019-10-21 20:40:35 +0200

    commit

--- exercice_6/index.js
+++ exercice_6/index.js
@@ -1,2 +1,18 @@
 'use strict'
 
+/*
+*
+*/
+
+function checkPhoneNumber(){
+	var phoneNumber = document.getElementById('phoneNumber').value;
+	var phoneNumberRGEX = /^(01|06|07)+[1-9][0-9]{8}$/;
+	var phoneNumberResult = phoneNumberRGEX.test(phoneNumber);
+
+
+	if (phoneNumberResult == true){
+		
+	}else{
+
+	}
+}
