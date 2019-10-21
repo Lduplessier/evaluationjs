ae91fd77ee550efd222d2194a2dc39f54febc3e2
LDU <lucas.duplessier@gmail.com>
2019-10-21 19:49:15 +0200

    commit

--- exercice_5/index.js
+++ exercice_5/index.js
@@ -1,2 +1,32 @@
 'use strict'
 
+function validate(){
+  var firstName = document.getElementById('firstName').value;
+  var lastName = document.getElementById('lastName').value;
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  var firstNameRGEX = /^[a-zA-Z]$/;
+  var lastNameRGEX = /^[a-zA-Z]$/;
+  var emailRGEX = /^[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\@[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*\.[a-zA-Z0-9_]{2.4}$/;
+  var passwordRGEX = /^[a-zA-Z0-9_]$/;
+  var firstNameResult = firstNameRGEX.test(firstName);
+  var lastNameResult = lastNameRGEX.test(lastName);
+  var emailResult = emailRGEX.test(email);
+  var passwordResult = passwordRGEX.test(password);
+
+  if (firstNameResult == false){
+  	
+  };
+
+  if (lastNameResult == false){
+  	
+  };
+
+  if (emailResult == false){
+  	
+  };
+
+  if (passwordResult == false){
+  	
+  }
+}
