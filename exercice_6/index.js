5f21c028b99f4f2a8dd8f5039b546d7799da8c05
LDU <lucas.duplessier@gmail.com>
2019-10-27 16:15:20 +0100

    Update index.js

--- exercice_6/index.js
+++ exercice_6/index.js
@@ -1,18 +1,22 @@
 'use strict'
 
 /*
-*
+*check phone number
 */
 
 function checkPhoneNumber(){
 	var phoneNumber = document.getElementById('phoneNumber').value;
-	var phoneNumberRGEX = /^(01|06|07)+[1-9][0-9]{8}$/;
+	var phoneNumberRGEX = /^(01|06|07)\ [1-9][0-9]{8}$/;
 	var phoneNumberResult = phoneNumberRGEX.test(phoneNumber);
 
 
-	if (phoneNumberResult == true){
-		
-	}else{
+/*
+*return result
+*/
 
+	if (phoneNumberResult == true){
+		console.log('valid phone number')
+	}else{
+		console.log('unvalid phone number')
 	}
 }
